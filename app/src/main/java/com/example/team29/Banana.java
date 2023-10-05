d0304e2e931188e244c42b72f7abfcfef22ca7f0
Wenjian(Michael) Shi <61084115+Micsushi@users.noreply.github.com>
2023-10-05 11:55:31 -0600

    Reverted my changes
    I messed up my branch so I will redo it and the adding color to master

--- app/src/main/java/com/example/team29/Banana.java
+++
@@ -1,4 +0,0 @@
-package com.example.team29;
-
-public class Banana extends Shape{
-}
--- app/src/main/java/com/example/team29/Shape.java
+++ app/src/main/java/com/example/team29/Shape.java
@@ -3,6 +3,4 @@ package com.example.team29;
 public abstract class Shape {
     int x;
     int y;
-
-    String color = "White";
 }
